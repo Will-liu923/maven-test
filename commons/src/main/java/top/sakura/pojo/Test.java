e546f6d09e4d254eb503445ff042f1f7dbb0bef8
Will Liu <leoLW923@outlook.com>
2021-08-20 12:28:58 +0800

    test maven

---
+++ commons/src/main/java/top/sakura/pojo/Test.java
@@ -0,0 +1,12 @@
+package top.sakura.pojo;
+
+/**
+ * @author leoLW
+ * @Description
+ * @create 2021-08-20 12:26
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("maven test");
+    }
+}
